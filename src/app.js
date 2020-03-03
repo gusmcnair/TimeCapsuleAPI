b2da185657c6e4b66e669fca7f78513bec4b02d1
Angus McNair <amcnair234@gmail.com>
2020-03-02 20:57:38 -0600

    Edited timezones

--- src/app.js
+++ src/app.js
@@ -33,14 +33,14 @@ const serializeCapsuleGetById = (capsule) => ({
   id: capsule.id,
   contents: xss(capsule.contents),
   imageurl: xss(capsule.imageurl),
-  opendates: moment.parseZone(capsule.opendates).format('LLL')
+  opendates: moment.utc(capsule.opendates).format('LLL')
 })
 
 const serializeCapsuleGet = (capsule) => ({
   id: capsule.id,
   title: xss(capsule.title),
-  burydate: moment.parseZone(capsule.burydate).format('LLL'),
-  opendates: moment.parseZone(capsule.opendates).format('LLL'),
+  burydate: moment.utc(capsule.burydate).format('LLL'),
+  opendates: moment.utc(capsule.opendates).format('LLL'),
 })
 /*
 app.use(
