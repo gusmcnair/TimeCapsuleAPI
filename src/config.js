e9292ebfa132b9bfa068c1c4e70ca4a207149c6d
Angus McNair <amcnair234@gmail.com>
2021-07-16 17:44:19 -0500

    Modified config file

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL,
     API_TOKEN: process.env.API_TOKEN,
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN
+    CLIENT_ORIGIN: "https://timecapsule.vercel.app"
   }
